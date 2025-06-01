5998451c6e80fde83c55153a5908a60b2e2398b2
Aman Gupta <113699703+gamc12world@users.noreply.github.com>
2025-06-01 13:26:25 +0545

    Updated stripe.ts

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,6 +1,6 @@
 import { loadStripe } from '@stripe/stripe-js';
 
 // Replace with your Stripe publishable key
-const stripePromise = loadStripe('pk_test_51OpLZmBH5Qz41234567890abcdefghijklmnopqrstuvwxyz');
+const stripePromise = loadStripe('pk_test_51RV6TMRjmScj5GX7ZE6FwvgTxCZtBKZCmOMGvTxyuw6vIBi8YTE5A423NIt12V9xzwz2CIGHAp2LaAEDF6TWJ6xj00YjyHUkz3');
 
 export default stripePromise;
